986a39e6e898de8a3d6738ef17844a4b0a3668a0
Joseph Mar Carrillo <carrillo.josephmar@gmail.com>
2023-06-18 21:59:18 +0800

    added home logo

--- stacktrek-site/src/App.jsx
+++ stacktrek-site/src/App.jsx
@@ -8,7 +8,11 @@ function App() {
     <>
       <Header />
       <Navbar />
-      <main></main>
+      <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+        </h1>
+      </main>
       <Footer />
     </>
   );
--- stacktrek-site/src/component/Header.jsx
+++ stacktrek-site/src/component/Header.jsx
@@ -1,11 +1,13 @@
 import "./Header";
 import StackLogo from "../assets/images/stacktrek logo.png";
 import "./Header.css";
+import { IoHomeOutline } from "react-icons/io5";
 
 const Header = () => {
   return (
     <header>
       <img src={StackLogo} className="mr-3 h-8 sm:h-9" alt="StackTrek Logo" />
+      <span className="mr-5"><IoHomeOutline /></span>
       <span>STACKLEAGUE</span>
       <span>MY COURSES</span>
     </header>
